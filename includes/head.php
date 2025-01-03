af8ec922957a1fd4b92a6dbecac8213fae1ece7c
muzammil-umar-cd <muzammil.coredigitals@gmail.com>
2025-01-04 04:59:57 +0500

    check-git-commit

--- includes/head.php
+++ includes/head.php
@@ -24,7 +24,7 @@ include ('metadata.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
-    <?php echo $title; ?>
+    <?php echo $title; ?>.
     <?php echo $meta_title ; ?>
     <?php echo $meta_description ; ?>
     <?php echo $canonical ; ?>
